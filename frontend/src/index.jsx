d29d7634545a99e8e5de0fd799ef31a5b75cd743
Lucas Vitoria Alves <lucasvitoriaalves@hotmail.com>
2019-10-31 22:15:35 -0300

    componentes

---
+++ frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import  ReactDOM from 'react-dom'
+import App from './main/app'
+// import Todo from './todo/todo'
+ReactDOM.render(<App /> , document.getElementById('app'))
