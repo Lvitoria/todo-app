9b8dd9a0868c81398fb1e4ef2680204590fe41d4
Lucas Vitoria Alves <lucasvitoriaalves@hotmail.com>
2019-11-04 18:06:28 -0300

    todo form/list

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import PageHeader from '../template/pageHeader'
 export default (props) =>(
 
     <div>
-        about
+        <PageHeader name="Sobre" smal="Nós" /> 
+        
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam dolore commodi non natus laudantium. Quo quasi in voluptatibus excepturi, similique aliquam ipsa impedit debitis commodi recusandae officiis porro dicta consectetur!</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quos enim autem cupiditate totam placeat sed harum a minus, amet temporibus tenetur, earum natus voluptatibus velit numquam. Id, nihil eum.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam vero error labore ipsum, cum nam porro! Magni autem officiis dignissimos! Quaerat enim doloremque explicabo tenetur animi nemo recusandae molestias atque.</p>
     </div>
 )
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoFom'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                todo
+                <PageHeader name="Tarefas" small="Cadastro" />
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
