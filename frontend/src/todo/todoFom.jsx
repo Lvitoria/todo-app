78562c83ea2f8751a97150abe2e2603209161439
Lucas Vitoria Alves <lucasvitoriaalves@hotmail.com>
2019-11-07 17:54:45 -0300

    teclas de atalhos

--- frontend/src/todo/todoFom.jsx
+++ frontend/src/todo/todoFom.jsx
@@ -1,40 +1,57 @@
 import React from 'react'
+
 import Grid from '../template/grid'
 import  IconButton from '../template/iconButton'
-export default props =>(
-    <div role="form" className='todoForm'>
-       
-        <Grid cols='12 9 10'>
-            <input 
-                type="text" 
-                className="form-control" 
-                id="description" 
-                placeholder="Adicione uma tarefa"
-                onChange={props.listen}
-                value={props.description}
-            />
-        </Grid>
+
+
+export default props =>{
+    //entre adiciona
+    //shitf e entre pesquisa
+    //esc limpa os campos
+    const keyHandler = (e) =>{
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.search() : props.handleAdd()
+        }else if(e.key === 'Escape') {
+            props.handleAdd()
+        }
+    }
+    
+    return (
+        <div role="form" className='todoForm'>
         
-        <Grid cols='12 3 2'>
-            <IconButton 
-                 style='primary'
-                 icone="plus" 
-                 event={props.handleAdd}
-            />
+            <Grid cols='12 9 10'>
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    id="description" 
+                    placeholder="Adicione uma tarefa"
+                    onKeyUp ={keyHandler}
+                    onChange={props.listen}
+                    value={props.description}
+                />
+            </Grid>
+            
+            <Grid cols='12 3 2'>
+                <IconButton 
+                    style='primary'
+                    icone="plus" 
+                    event={props.handleAdd}
+                />
 
-            <IconButton 
-                 style='info'
-                 icone="search" 
-                 event={props.search}
-            />
+                <IconButton 
+                    style='info'
+                    icone="search" 
+                    event={props.search}
+                />
 
-            <IconButton 
-                 style='default'
-                 icone="close" 
-                 event={props.clear}
-            />
+                <IconButton 
+                    style='default'
+                    icone="close" 
+                    event={props.clear}
+                />
 
-        </Grid>
-    </div>
+            </Grid>
+        </div>
 
-)
+    )
+}
